71cc9149e2746a62e11edbe177a1b3241c1bd074
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2026-02-26 23:12:17 +0200

    fixed peekable.py

--- src/peekable/peekable.py
+++ src/peekable/peekable.py
@@ -3,16 +3,25 @@ from typing import Any
 
 class Peekable:
     def __init__(self, cont: list) -> None:
+        if type(cont) != list:
+            raise TypeError("Peekable must take a list.")
         self._contents = cont.copy()
         self._i = 0
+
 
     def items_left(self) -> int:
         return len(self._contents) - self._i
 
+
     def peek(self, count: int=1) -> Any | None:
-        if length <= self.items_left():
+        if count > self.items_left():
+            return None
+        elif count <= self.items_left() and count > 1:
             return self._contents[self._i:self._i+count]
-        return None
+        elif count == 1:
+            return self._contents[self._i]
+        else:
+            raise ValueError(f"Can't peek under 1 items.")
 
     def advance(self, count: int=1) -> Any | None:
         value = self.peek(count)
