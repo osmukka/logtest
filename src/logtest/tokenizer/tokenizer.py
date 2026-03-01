88ed6788b13e7a0169c38a98603af20b81e03561
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2026-03-01 21:19:36 +0200

    Added operators

--- src/logtest/tokenizer/tokenizer.py
+++ src/logtest/tokenizer/tokenizer.py
@@ -1,4 +1,20 @@
 from peekable.peekable import Peekable
+from tokens import Token
+from token_kinds import TokenKind
+
+
+operators = {
+    "-": TokenKind.Not,
+    "&&": TokenKind.And,
+    "AND": TokenKind.And,
+    "||": TokenKind.Or,
+    "OR": TokenKind.Or,
+    "->": TokenKind.Impl,
+    "IMPLIES": TokenKind.Impl,
+    "<->": TokenKind.Iff,
+    "IFF": TokenKind.Iff,
+    "=": TokenKind.Assign
+}
 
 
 class Tokenizer:
@@ -7,6 +23,7 @@ class Tokenizer:
         self.output = []
 
 
+    def tokenize_operator(self) -> None:
 
 
 
