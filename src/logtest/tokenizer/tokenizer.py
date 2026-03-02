5e94421b2ab688dc3818e2a767e256ab586399af
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2026-03-02 15:21:28 +0200

    Implemented tokenize_identifier and tokenize

--- src/logtest/tokenizer/tokenizer.py
+++ src/logtest/tokenizer/tokenizer.py
@@ -25,12 +25,35 @@ class Tokenizer:
         self._operator_lengths = list({len(key) for key in operators}).sorted(reverse=True)
 
 
+    def _tokenizer_identifier(self) -> None:
+        identifier_length = 1
+        while self._input.peek(identifier_length).isalpha():
+            identifier_length += 1
+        identifier_length -= 1
+
+        identifier_string = self._input.consume(identifier_length)
+        self._output.append(Token(TokenKind.Prep, identifier_string))
+
+
+
     def _tokenize_operator(self) -> None:
         for length in self._operator_lengths:
             op = self._input.peek(length)
             if op in operators:
+                self._input.consume(length)
                 self._output.append(Token(op))
-                return
+            
+    def tokenize(self, chars: list[str]) -> list[Token]:
+        self._input = Peekable(chars)
+        self._output = []
+
+        while self._input.peek():
+            if self._input.peek().isalpha():
+                self._tokenize_identifier()
+            else:
+                self._tokenize_operator()
+        return self._output()
+            
         
 
 
