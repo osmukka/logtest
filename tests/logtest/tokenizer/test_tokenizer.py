79edaccc662cb495034991522a1040f76ddad550
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2026-03-04 20:13:36 +0200

    Simplified tests

--- tests/logtest/tokenizer/test_tokenizer.py
+++ tests/logtest/tokenizer/test_tokenizer.py
@@ -6,37 +6,24 @@ from logtest.tokenizer.token_kinds import TokenKind
 
 
 @pytest.fixture
-def new_tokenizer():
+def tokenizer():
     return Tokenizer()
 
 
 class TestTokenize:
-    def test_tokenize_individual_operators(self, new_tokenizer):
-        assert new_tokenizer.tokenize("-") == [Token(TokenKind.Not)]
-        assert new_tokenizer.tokenize("&&") == [Token(TokenKind.And)]
-        assert new_tokenizer.tokenize("||") == [Token(TokenKind.Or)]
-        assert new_tokenizer.tokenize("->") == [Token(TokenKind.Impl)]
-        assert new_tokenizer.tokenize("<->") == [Token(TokenKind.Iff)]
-        assert new_tokenizer.tokenize("=") == [Token(TokenKind.Assign)]
+    def test_tokenize_operators(self, tokenizer):
+        assert tokenizer.tokenize("-") == [Token(TokenKind.Not)]
+        assert tokenizer.tokenize("&&") == [Token(TokenKind.And)]
+        assert tokenizer.tokenize("||") == [Token(TokenKind.Or)]
+        assert tokenizer.tokenize("->") == [Token(TokenKind.Impl)]
+        assert tokenizer.tokenize("<->") == [Token(TokenKind.Iff)]
+        assert tokenizer.tokenize("=") == [Token(TokenKind.Assign)]
+        assert tokenizer.tokenize("(") == [Token(TokenKind.LParen)]
+        assert tokenizer.tokenize(")") == [Token(TokenKind.RParen)]
 
-    def test_tokenize_all_operators(self, new_tokenizer):
-        operator_tokens = new_tokenizer.tokenize("-&&||-><->=")
-        assert operator_tokens[0] == Token(TokenKind.Not)
-        assert operator_tokens[1] == Token(TokenKind.And)
-        assert operator_tokens[2] == Token(TokenKind.Or)
-        assert operator_tokens[3] == Token(TokenKind.Impl)
-        assert operator_tokens[4] == Token(TokenKind.Iff)
-        assert operator_tokens[5] == Token(TokenKind.Assign)
 
-    def test_tokenize_identifiers(self, new_tokenizer):
-        assert new_tokenizer.tokenize("True") == [Token(TokenKind.TruthVal, True)]
-        assert new_tokenizer.tokenize("False") == [Token(TokenKind.TruthVal, False)]
-        assert new_tokenizer.tokenize("test") == [Token(TokenKind.Prep, "test")]
-
-    def test_misc_expressions(self, new_tokenizer):
-        identifier_tokens = new_tokenizer.tokenize("A->B")
-        assert identifier_tokens[0] == Token(TokenKind.Prep, "A")
-        assert identifier_tokens[1] == Token(TokenKind.Impl)
-        assert identifier_tokens[2] == Token(TokenKind.Prep, "B")
-
+    def test_tokenize_identifiers(self, tokenizer):
+        assert tokenizer.tokenize("True") == [Token(TokenKind.TruthVal, True)]
+        assert tokenizer.tokenize("False") == [Token(TokenKind.TruthVal, False)]
+        assert tokenizer.tokenize("test") == [Token(TokenKind.Prep, "test")]
 
