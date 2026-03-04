e74f78f7fe806479df146cbf14d10b413565bfa0
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2026-03-04 16:17:15 +0200

    Started implementing tests for parser

---
+++ tests/logtest/parser/test_parser.py
@@ -0,0 +1,8 @@
+import pytest
+
+from logtest.parser.parser import Parser
+from logtest.parser.logtest_ast import AST_Node, AST_TerminalNode, AST_BinaryNode
+from logtest.tokenizer.tokenizer import Tokenizer
+from logtest.tokenizer.tokens import Tokenizer
+from logtest.tokenizer.token_kinds import Tokenizer
+
