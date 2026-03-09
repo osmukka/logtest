9f0e043491dbaadd2c24da3dfe4847efec7ec0aa
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2026-03-09 14:02:20 +0200

    Rewrote tokenizer unit tests

--- tests/logtest/unit/test_tokenizer.py
+++ tests/logtest/unit/test_tokenizer.py
@@ -5,26 +5,31 @@ from logtest.tokenizer.tokens import Token
 from logtest.tokenizer.token_kinds import TokenKind
 
 
-@pytest.fixture
-def tokenizer():
-    return Tokenizer()
+def tokenize(string: str) -> list[Token]:
+    return Tokenizer().tokenize(string)
 
 
 class TestTokenize:
-    def test_tokenize_operators(self, tokenizer):
-        assert tokenizer.tokenize("-")[0] == Token(TokenKind.Not)
-        assert tokenizer.tokenize("&&")[0] == Token(TokenKind.And)
-        assert tokenizer.tokenize("||")[0] == Token(TokenKind.Or)
-        assert tokenizer.tokenize("->")[0] == Token(TokenKind.Impl)
-        assert tokenizer.tokenize("<->")[0] == Token(TokenKind.Iff)
-        assert tokenizer.tokenize("=")[0] == Token(TokenKind.Assign)
-        assert tokenizer.tokenize("(")[0] == Token(TokenKind.LParen)
-        assert tokenizer.tokenize(")")[0] == Token(TokenKind.RParen)
-        assert tokenizer.tokenize("<")[0] == Token(TokenKind.Error, ("<", 0))
+    def test_tokenizing_operators(self):
+        assert tokenize("-")[0] == Token(TokenKind.Not)
+        assert tokenize("&&")[0] == Token(TokenKind.And)
+        assert tokenize("||")[0] == Token(TokenKind.Or)
+        assert tokenize("->")[0] == Token(TokenKind.Impl)
+        assert tokenize("<->")[0] == Token(TokenKind.Iff)
+        assert tokenize("=")[0] == Token(TokenKind.Assign)
+        assert tokenize("(")[0] == Token(TokenKind.LParen)
+        assert tokenize(")")[0] == Token(TokenKind.RParen)
+        assert tokenize("<")[0] == Token(TokenKind.Error, ("<", 0))
 
+    def test_tokenizing_identifiers(self):
+        assert tokenize("True")[0] == Token(TokenKind.Identifier, "True")
+        assert tokenize("False")[0] == Token(TokenKind.Identifier, "False")
+        assert tokenize("test")[0] == Token(TokenKind.Identifier, "test")
 
-    def test_tokenize_identifiers(self, tokenizer):
-        assert tokenizer.tokenize("True")[0] == Token(TokenKind.Identifier, "True")
-        assert tokenizer.tokenize("False")[0] == Token(TokenKind.Identifier, "False")
-        assert tokenizer.tokenize("test")[0] == Token(TokenKind.Identifier, "test")
+    def test_tokenizing_empty_string(self):
+        assert tokenize("") == [Token(TokenKind.EOF)]
+
+    def test_tokenizer_errors(self):
+        assert tokenize("1")[0] == Token(TokenKind.Error, ("1", 0))
+
 
